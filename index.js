970b56892b77ebc968a0e09addc3aec0ea37296a
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-06-21 08:33:23 -0400

    must fix the module call

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markDown = require ('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is your prohect title?',
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Successfully created index.html!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const data = markDown.generateMarkdown(answers);
+            writeToFile("README.md", data);
+        });
+
+}
 
 // Function call to initialize app
 init();
